6964ee02c01334af6686e4a0de9b364281c12d0b
Saxogoal <andrepiros01@gmail.com>
2026-03-10 21:36:33 -0300

    fixed the last modified

--- scripts/getdate.js
+++ scripts/getdate.js
@@ -1,8 +1,7 @@
-const copywrightYearElement = document.getElementById('currentyear');
-const lastModifiedElement = document.getElementById('lastModified');
+const copyrightYearElement = document.getElementById("currentyear");
+const lastModifiedElement = document.getElementById("lastModified");
 
 const currentYear = new Date().getFullYear();
-copywrightYearElement.textContent = currentYear;
+copyrightYearElement.textContent = currentYear;
 
-const lastModified = `Last Modification: ${document.lastModified}`;
-lastModifiedElement.textContent = lastModified;
+lastModifiedElement.textContent = `Last Modification: ${document.lastModified}`;
