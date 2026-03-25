8cd16a834c721cbea8aec11433735f1be9763b36
Saxogoal <andrepiros01@gmail.com>
2026-03-25 11:27:02 -0300

    a

--- scripts/place.js
+++ scripts/place.js
@@ -7,13 +7,13 @@ document.lastModified;
 const temp = 10;
 const wind = 5;
 
-function calculateWindChill(t, v){
-return 13.12 + 0.6215*t - 11.37*Math.pow(v,0.16) + 0.3965*t*Math.pow(v,0.16);
+function calculateWindChill(t, v) {
+return 13.12 + 0.6215 * t - 11.37 * Math.pow(v, 0.16) + 0.3965 * t * Math.pow(v, 0.16);
 }
 
 let windChill = "N/A";
 
-if (temp <= 10 && wind > 4.8){
+if (temp <= 10 && wind > 4.8) {
 windChill = calculateWindChill(temp, wind).toFixed(1) + " °C";
 }
 
