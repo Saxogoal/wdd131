94ff8508279e671193b1220fae6bc052f08769b0
Saxogoal <andrepiros01@gmail.com>
2026-04-07 21:02:22 -0300

    more adjustments

--- scripts/form.js
+++ scripts/form.js
@@ -1,8 +1,30 @@
 //Product Name
 const products = [
-    { name: "Product A", price: 10.99 },
-    { name: "Product B", price: 24.99 },
-    { name: "Product C", price: 5.99 }
+{
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+},
+{
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+},
+{
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+},
+{
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+},
+{
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+}
 ];
 
 const select = document.querySelector("#productName");
@@ -13,6 +35,16 @@ products.forEach(product => {
     option.textContent = product.name;
     select.appendChild(option);
 });
+
+// Review Count
+let reviewCount = Number(window.localStorage.getItem("reviewCount-ls")) || 0;
+reviewCount++;
+localStorage.setItem("reviewCount-ls", reviewCount);
+
+const countDisplay = document.querySelector("#review-count");
+if (countDisplay) {
+    countDisplay.textContent = reviewCount;
+}
 
 // Footer
 document.querySelector("#currentyear").textContent = new Date().getFullYear();
