0e46d0f8571e5f92769e82666c319d65515c1423
Saxogoal <andrepiros01@gmail.com>
2026-04-09 17:58:16 -0300

    fixed the hamburguer button

--- project/scripts/backlog.js
+++ project/scripts/backlog.js
@@ -1,2 +1,13 @@
+// Hamburger menu
+const menu = document.querySelector("#menu");
+const navigation = document.querySelector(".nav-links");
+
+menu.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    menu.classList.toggle("open");
+});
+
+
+// Footer
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
